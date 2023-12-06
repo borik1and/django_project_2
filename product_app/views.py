33f0de43dfab19e5ace83cbf07a806b718da5580
Boris <borik1and@gmail.com>
2023-12-06 16:41:08 +0200

    add class AboutView

--- product_app/views.py
+++ product_app/views.py
@@ -1,18 +1,12 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView, CreateView
 from product_app.models import Product
+from django.views import View
 
 
 class IndexListView(ListView):
     model = Product
-    template_name = 'product_app/index.html'
-
-# def index(request):
-#     context = {
-#         'object_list': Product.objects.all(),
-#         'title': 'Главная'
-#     }
-#     return render(request, 'product_app/index.html', context)
+    template_name = 'product_app/index_list.html'
 
 
 class CatalogListView(ListView):
@@ -20,14 +14,18 @@ class CatalogListView(ListView):
     template_name = 'product_app/catalog_list.html'
 
 
-def about(request):
-    if request.method == 'POST':
-        # в переменной request хранится информация о методе, который отправлял пользователь
+class AboutView(View):
+    template_name = 'product_app/about.html'
+
+    def post(self, request, *args, **kwargs):
         email = request.POST.get('email')
         name = request.POST.get('name')
         message = request.POST.get('message')
-        # а также передается информация, которую заполнил пользователь
+
         print(f'имя: {name}, имеил: {email}, сообщение: {message}.')
         with open('message.txt', 'a') as t_f:
             t_f.write(f'имя: {name}, имеил: {email}, сообщение: {message}.')
-    return render(request, 'product_app/about.html')
+        return render(request, 'product_app/about.html')
+
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name)
