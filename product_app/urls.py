c5aab418b58d43a3aa265038ab8c37f82d7e5318
Boris <borik1and@gmail.com>
2023-12-06 16:40:40 +0200

    add code line

--- product_app/urls.py
+++ product_app/urls.py
@@ -1,16 +1,13 @@
-
 from django.urls import path
-
+from product_app.views import CatalogListView, IndexListView, AboutView
 from product_app import views
-from product_app.apps import PtoductAppConfig
+# from product_app.apps import ProductAppConfig
 
-app_name = PtoductAppConfig.name
-
+# app_name = ProductAppConfig.name
 
 urlpatterns = [
-    path('', views.index),
-    path('index', views.index),
-    path('about', views.about),
-    path('catalog', views.catalog),
-
+    path('', IndexListView.as_view(), name='index'),
+    path('index', IndexListView.as_view(), name='index'),
+    path('about', AboutView.as_view(), name='about'),
+    path('catalog', CatalogListView.as_view(), name='catalog'),
 ]
