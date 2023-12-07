83879e01ca36d7297d88a10e15a3f2c43eda83d4
Boris <borik1and@gmail.com>
2023-12-07 17:26:53 +0200

    add code update

--- blog_app/views.py
+++ blog_app/views.py
@@ -1,14 +1,22 @@
 from django.shortcuts import render
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 from django.views.generic import CreateView, ListView, DetailView, UpdateView, DeleteView
 from blog_app.models import Blog_app
-
+from pytils.translit import slugify
 
 class Blog_appCreateView(CreateView):
     model = Blog_app
     fields = ('title', 'body', 'creation_date', 'publication', 'preview')
     # template_name = 'blog_app/blog_app_form.html'
     success_url = reverse_lazy('blog_app:list')
+
+    def form_valid(self, form):
+        if form.is_valid():
+            new_mat = form.save()
+            new_mat.slug = slugify(new_mat.title)
+            new_mat.save()
+
+        return super().form_valid(form)
 
 
 class Blog_appListView(ListView):
@@ -34,8 +42,19 @@ class Blog_appUpdateView(UpdateView):
     model = Blog_app
     fields = ('title', 'body', 'creation_date', 'publication')
     template_name = 'blog_app/blog_app_form.html'
-    success_url = reverse_lazy('blog_app:list')
+    # success_url = reverse_lazy('blog_app:list')
     queryset = Blog_app.objects.all()
+
+    def form_valid(self, form):
+        if form.is_valid():
+            new_mat = form.save()
+            new_mat.slug = slugify(new_mat.title)
+            new_mat.save()
+
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('blog_app:view', args=[self.kwargs.get('pk')])
 
 
 class Blog_appDeleteView(DeleteView):
